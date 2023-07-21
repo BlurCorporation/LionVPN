c8aa2f47cae167128486568650381513a02ab92d
1inchunikhin <1inchunikhin@gmail.com>
2023-07-21 18:33:59 +0300

    first commit

---
+++ LionVPN/LionVPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  LionVPNApp.swift
+//  LionVPN
+//
+//  Created by Николай Чунихин on 21.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct LionVPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
