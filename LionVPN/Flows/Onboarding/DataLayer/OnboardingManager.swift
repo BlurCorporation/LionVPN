7af001766bac6aafa50eb46661abf5b992c9648e
1inchunikhin <1inchunikhin@gmail.com>
2023-09-11 17:19:20 +0300

    Add OnboardingModel and OnboardingManager in data layer

---
+++ LionVPN/Flows/Onboarding/DataLayer/OnboardingManager.swift
@@ -0,0 +1,35 @@
+//
+//  OnboardingManager.swift
+//  LionVPN
+//
+//  Created by Николай Чунихин on 08.09.2023.
+//
+
+import SwiftUI
+
+//MARK: - Protocol
+protocol OnboardingManagerProtocol {
+    /// Используется для сохранения информации о прохождении Онбординга
+    func setOnboardingCompleted()
+    /// Используется для получения информации о прохождении Онбординга
+    /// - Returns: Пройден ли онбординг
+    func isOnboardingComplete() -> Bool
+}
+
+//MARK: - Manager
+final class OnboardingManager {
+    /// Переменная в UserDefault, хранящая информацию о прохождении онбординга
+    @AppStorage("onboarding") private var isCompleted = false
+}
+
+//MARK: - extensions
+extension OnboardingManager: OnboardingManagerProtocol {
+    
+    func setOnboardingCompleted() {
+        isCompleted = true
+    }
+    
+    func isOnboardingComplete() -> Bool {
+        return isCompleted
+    }
+}
---
+++ LionVPN/Flows/Onboarding/DataLayer/OnboardingModel.swift
@@ -0,0 +1,13 @@
+//
+//  OnboardingModel.swift
+//  LionVPN
+//
+//  Created by Николай Чунихин on 07.09.2023.
+//
+
+/// Структура, которая содержит данные для слоя представления онбординга
+struct OnboardingContent {
+    let imageName: String
+    let title: String
+    let description: (String, String)
+}
